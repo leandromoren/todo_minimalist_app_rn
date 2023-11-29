10ce834c113f3646fcc5ec6332757d3e07be502f
leandromoren <leandro.moren@outlook.com>
2023-11-29 11:12:24 -0300

    creo un componenten que contiene un arreglo con diferentes objetos

---
+++ data/todos.js
@@ -0,0 +1,32 @@
+const todosData = [
+    {
+        id: 1,
+        title: 'Take out the trash',
+        isCompleted: true,
+        isToday: true,
+        hour:  '10:00'
+    },
+    {
+        id: 2,
+        title: 'Meeting with the pibes',
+        isCompleted: false,
+        isToday: false,
+        hour:  '10:30'
+    },
+    {
+        id: 3,
+        title: 'Dentista',
+        isCompleted: true,
+        isToday: true,
+        hour:  '15:20'
+    },
+    {
+        id: 4,
+        title: 'Sacar a pasear a los perros',
+        isCompleted: false,
+        isToday: true,
+        hour:  '18:00'
+    },
+]
+
+export default todosData;
